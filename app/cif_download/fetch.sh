c22cc7c58edb88d56ff223024fb9cd6aa7d4a299
joth.moss@googlemail.com <joth.moss@googlemail.com>
2023-05-25 11:29:56 +0100

    CIF download dockerised

--- app/cif_download/fetch.sh
+++ app/cif_download/fetch.sh
@@ -14,8 +14,18 @@ function get_date {
   echo $(head -n 1 $1 | awk '{print $1}' | grep -Eo 'PD[0-9]{6}' | sed 's/PD//')
 }
 
+function touch_time {
+  
+  DT=$1
+  day="${DT:4:2}"
+  month="${DT:2:2}"
+  year="${DT:0:2}"
+  echo $day$month$year
+}
+
 # Download the latest FULL CIF & un-gzip
 FILE="CIF_ALL_FULL_DAILY.CIF"
+echo "Downloading $FILE"
 curl -L -u $NROD_USER:$NROD_PASS -o $FILE.gz "$URL?type=CIF_ALL_FULL_DAILY&day=toc-full.CIF.gz"
 
 if gzip -t $FILE.gz; then
@@ -27,42 +37,56 @@ fi
 
 # Get the file reference
 REF=$(get_ref $FILE)
+echo "File reference: $REF"
 
 # Get the file date
 FULL_DATE=$(get_date $FILE)
+# echo "File date: $FULL_DATE"
+
+TOUCH=$(touch_time $FULL_DATE)
+echo "Full CIF File date: $TOUCH"
 
 # Rename the full CIF and adjust the file date
 mv $FILE "$REF.CIF"
-touch "$REF.CIF" -d $FULL_DATE
+touch "$REF.CIF" -t $TOUCH
+echo "Saved $REF.CIF to $(pwd)"
 
+echo "Looking for incremental CIF"
 DAYS=("sat" "sun" "mon" "tue" "wed" "thu")
 for DAY in ${DAYS[@]}; do
-  FILE="toc-update-$DAY"
-  curl -L -u $NROD_USER:$NROD_PASS -o $FILE.gz "$URL?type=CIF_ALL_UPDATE_DAILY&day=$FILE.CIF.gz"
+  INC_FILE="toc-update-$DAY"
+  curl -L -u $NROD_USER:$NROD_PASS -o $INC_FILE.gz "$URL?type=CIF_ALL_UPDATE_DAILY&day=$INC_FILE.CIF.gz"
   
-  if gzip -t $FILE.gz; then
-    gzip -d $FILE.gz
+  if gzip -t $INC_FILE.gz; then
+    gzip -d $INC_FILE.gz
   else
-    rm $FILE.gz
+    rm $INC_FILE.gz
     continue
   fi
 
+  echo "Processing $INC_FILE"
   # Get the file reference
-  REF=$(get_ref $FILE)
+  REF=$(get_ref $INC_FILE)
+  echo "File reference: $REF"
 
   # Get the file date
-  DATE=$(get_date $FILE)
+  DATE=$(get_date $INC_FILE)
+  echo "File date: $(touch_time $DATE)"
 
   # Compare dates
-  FULL_CIF_DATE=$(date -d $FULL_DATE +"%Y%m%d")
-  UPDATE=$(date -d $DATE +"%Y%m%d")
+  FULL_CIF_DATE=$(date -d $TOUCH +"%Y%m%d")
+  UPDATE=$(date -d $(touch_time $DATE) +"%Y%m%d")
+
   if [ $FULL_CIF_DATE -ge $UPDATE ];
   then
-    rm $FILE
+    echo "Deleting $INC_FILE, date expired"
+    rm $INC_FILE
   else
-    mv $FILE "$REF.CIF"
-    touch "$REF.CIF" -d $DATE
+    mv $INC_FILE "$REF.CIF"
+    touch "$REF.CIF" -t $(touch_time $DATE)
+    echo "Saved $REF.CIF to $(pwd)"
   fi
 
 done
+echo "Finished"
 
