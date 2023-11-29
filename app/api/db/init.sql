2c7cfb2389070c018318bd8b1f7c22402f1d5cdd
joth.moss@googlemail.com <joth.moss@googlemail.com>
2023-11-29 12:50:15 +0000

    Added more init.sql

--- app/api/db/init.sql
+++ app/api/db/init.sql
@@ -1,5 +1,164 @@
 -- To be imported on database initialisation...
 
+DROP TABLE IF EXISTS "basic_extra";
+DROP SEQUENCE IF EXISTS basic_extra_id_seq;
+CREATE SEQUENCE basic_extra_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."basic_extra" (
+    "id" integer DEFAULT nextval('basic_extra_id_seq') NOT NULL,
+    "bs_id" integer,
+    "uic_code" character varying(5),
+    "atoc_code" character varying(2) NOT NULL,
+    "applicable_timetable" character varying(1) NOT NULL,
+    CONSTRAINT "basic_extra_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+CREATE INDEX "ix_basic_extra_bs_id" ON "public"."basic_extra" USING btree ("bs_id");
+
+CREATE INDEX "ix_basic_extra_id" ON "public"."basic_extra" USING btree ("id");
+
+
+DROP TABLE IF EXISTS "basic_schedule";
+DROP SEQUENCE IF EXISTS basic_schedule_id_seq;
+CREATE SEQUENCE basic_schedule_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."basic_schedule" (
+    "id" integer DEFAULT nextval('basic_schedule_id_seq') NOT NULL,
+    "cif_header" integer NOT NULL,
+    "transaction_type" character varying(1) NOT NULL,
+    "uid" character varying(6) NOT NULL,
+    "date_runs_from" character varying(6) NOT NULL,
+    "date_runs_to" character varying(6),
+    "days_run" character varying(7),
+    "bank_holiday_running" character varying(1),
+    "train_status" character varying(1),
+    "train_category" character varying(2),
+    "train_identity" character varying(4),
+    "headcode" character varying(4),
+    "train_service_code" character varying(8),
+    "portion_id" character varying(1),
+    "power_type" character varying(3),
+    "timing_load" character varying(4),
+    "speed" character varying(3),
+    "operating_characteristics" character varying(6),
+    "seating_class" character varying(1),
+    "sleepers" character varying(1),
+    "reservations" character varying(1),
+    "catering_code" character varying(4),
+    "service_branding" character varying(4),
+    "stp_indicator" character varying(1),
+    CONSTRAINT "basic_schedule_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+CREATE INDEX "ix_basic_schedule_date_runs_from" ON "public"."basic_schedule" USING btree ("date_runs_from");
+
+CREATE INDEX "ix_basic_schedule_date_runs_to" ON "public"."basic_schedule" USING btree ("date_runs_to");
+
+CREATE INDEX "ix_basic_schedule_days_run" ON "public"."basic_schedule" USING btree ("days_run");
+
+CREATE INDEX "ix_basic_schedule_id" ON "public"."basic_schedule" USING btree ("id");
+
+CREATE INDEX "ix_basic_schedule_uid" ON "public"."basic_schedule" USING btree ("uid");
+
+
+DROP TABLE IF EXISTS "changes_en_route";
+DROP SEQUENCE IF EXISTS changes_en_route_id_seq;
+CREATE SEQUENCE changes_en_route_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."changes_en_route" (
+    "id" integer DEFAULT nextval('changes_en_route_id_seq') NOT NULL,
+    "bs_id" integer,
+    "tiploc" character varying(7) NOT NULL,
+    "suffix" integer,
+    "train_category" character varying(2),
+    "train_identity" character varying(4),
+    "headcode" character varying(4),
+    "train_service_code" character varying(8),
+    "portion_id" character varying(1),
+    "power_type" character varying(3),
+    "timing_load" character varying(4),
+    "speed" character varying(3),
+    "operating_characteristics" character varying(6),
+    "seating_class" character varying(1),
+    "sleepers" character varying(1),
+    "reservations" character varying(1),
+    "catering_code" character varying(4),
+    "service_branding" character varying(4),
+    "uic_code" character varying(5),
+    CONSTRAINT "changes_en_route_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+CREATE INDEX "ix_changes_en_route_bs_id" ON "public"."changes_en_route" USING btree ("bs_id");
+
+CREATE INDEX "ix_changes_en_route_id" ON "public"."changes_en_route" USING btree ("id");
+
+
+DROP TABLE IF EXISTS "header_record";
+DROP SEQUENCE IF EXISTS header_record_id_seq;
+CREATE SEQUENCE header_record_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."header_record" (
+    "id" integer DEFAULT nextval('header_record_id_seq') NOT NULL,
+    "mainframe_identity" character varying(20) NOT NULL,
+    "extract_date" character varying(10) NOT NULL,
+    "extract_time" character varying(8) NOT NULL,
+    "current_file_ref" character varying(7) NOT NULL,
+    "last_file_ref" character varying(7),
+    "update_indicator" character varying(1) NOT NULL,
+    "version" character varying(1) NOT NULL,
+    "user_start_date" character varying(10) NOT NULL,
+    "user_end_date" character varying(10) NOT NULL,
+    "compressed_size" bigint NOT NULL,
+    "uncompressed_size" bigint NOT NULL,
+    "archive_file_name" character varying NOT NULL,
+    "uncompressed_file_name" character varying NOT NULL,
+    "downloaded_datetime" character varying(19) NOT NULL,
+    "processed_datetime" character varying(19),
+    "status" status,
+    CONSTRAINT "header_record_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "ix_header_record_mainframe_identity" UNIQUE ("mainframe_identity")
+) WITH (oids = false);
+
+CREATE INDEX "ix_header_record_id" ON "public"."header_record" USING btree ("id");
+
+
+DROP TABLE IF EXISTS "location";
+DROP SEQUENCE IF EXISTS location_id_seq;
+CREATE SEQUENCE location_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."location" (
+    "id" integer DEFAULT nextval('location_id_seq') NOT NULL,
+    "bs_id" integer,
+    "record_type" character varying(2) NOT NULL,
+    "tiploc" character varying(7) NOT NULL,
+    "suffix" integer,
+    "wta" character varying(5),
+    "wtp" character varying(5),
+    "wtd" character varying(5),
+    "pta" character varying(4),
+    "ptd" character varying(4),
+    "platform" character varying(3),
+    "line" character varying(3),
+    "path" character varying(3),
+    "activity" character varying(12),
+    "engineering_allowance" character varying(2),
+    "pathing_allowance" character varying(2),
+    "performance_allowance" character varying(2),
+    CONSTRAINT "location_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+CREATE INDEX "ix_location_bs_id" ON "public"."location" USING btree ("bs_id");
+
+CREATE INDEX "ix_location_id" ON "public"."location" USING btree ("id");
+
+
+ALTER TABLE ONLY "public"."basic_extra" ADD CONSTRAINT "basic_extra_bs_id_fkey" FOREIGN KEY (bs_id) REFERENCES basic_schedule(id) ON DELETE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."changes_en_route" ADD CONSTRAINT "changes_en_route_bs_id_fkey" FOREIGN KEY (bs_id) REFERENCES basic_schedule(id) ON DELETE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."location" ADD CONSTRAINT "location_bs_id_fkey" FOREIGN KEY (bs_id) REFERENCES basic_schedule(id) ON DELETE CASCADE NOT DEFERRABLE;
+
+
 -- Stored Procedure to process replace transactions
 DROP PROCEDURE IF EXISTS process_rep();
 CREATE OR REPLACE PROCEDURE process_rep() AS
